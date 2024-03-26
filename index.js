d079a42634d9eef958efa743be90dd29b6d36556
jintingamy <2270544121@qq.com>
2024-03-25 19:35:03 -0700

    second

--- index.js
+++ index.js
@@ -24,7 +24,13 @@ const app = express()
 // 自己定义的私钥
 const secret = 'dssffdfcdd'
 // 跨域访问
-app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+// app.use(cors())
 // 使用json跟urlencoded中间件接收post传过来的json数据，放在req.body上
 app.use(express.json())
 app.use(express.urlencoded({extends:false}))
